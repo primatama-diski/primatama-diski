be3886de29411adb2cc2e2b74eb2f98ed5cd3b07
primatama-diski <116284139+primatama-diski@users.noreply.github.com>
2022-11-20 20:54:43 +0700

    Add files via upload

---
+++ penjumlahan 2 bilangan hasil sama .py
@@ -0,0 +1,8 @@
+a = int(input('masukan bilangan a: '))
+b = int(input('masukan bilangan b: '))
+c = int(input('masukan bilangan c: '))
+
+if a+b == c or b+c == a or c+a == b:
+    print('benar')
+else:
+    print('salah')
